a7d6674256013ae8c80aab0e325f70f5db5a726b
Kyle Smyth <kylesmyth@gmail.com>
2013-09-28 10:24:11 -0600

    Ability to add a conference

---
+++ client/views/add_conference.js
@@ -0,0 +1,38 @@
+Template.addConference.helpers({
+
+});
+
+Template.addConference.events({
+	'submit form': function(ev)
+	{
+		ev.preventDefault();
+
+		var user = Meteor.user();
+		if(user)
+		{
+			var newConference = {
+				owner: user._id,
+				name: $(ev.target).find('[id=name]').val(),
+				categories: [],
+				speakers: [],
+				sessions: []
+			}
+
+			Meteor.call('addConference', newConference, function(error, id)
+			{
+				if(error)
+				{
+					console.log(error);
+				}
+				else
+				{
+					Meteor.Router.to('viewConference', id);
+				}
+			});
+		}
+		else
+		{
+			console.log('NOT LOGGED IN');
+		}
+	}
+});
