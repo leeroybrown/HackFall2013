4ea34ca84cd5db411b999d59ddff7872cbd839ac
Kyle Smyth <kylesmyth@gmail.com>
2013-09-28 11:32:09 -0600

    Adding speakers to a conference

---
+++ client/views/add_speaker.js
@@ -0,0 +1,41 @@
+Template.addSpeaker.events({
+	'submit form': function(ev)
+	{
+		ev.preventDefault();
+
+		var user = Meteor.user();
+		var confId = Session.get('conferenceId');
+		console.log(confId);
+		var conference = Conferences.findOne({_id: confId});
+		if(user && conference)
+		{
+			var newSpeaker = {
+				conferenceId: confId,
+				name: $(ev.target).find('[id=name]').val(),
+				location: $(ev.target).find('[id=location]').val(),
+				blog: $(ev.target).find('[id=blog]').val(),
+				website: $(ev.target).find('[id=website]').val(),
+				twitter: $(ev.target).find('[id=twitter]').val(),
+				details: $(ev.target).find('[id=details]').val(),
+			}
+
+			Meteor.call('addSpeakerToConference', newSpeaker, function(error, id)
+			{
+				if(error)
+				{
+					console.log(error);
+				}
+				else
+				{
+					console.log(id);
+					//Meteor.Router.to('addSpeaker', id);
+				}
+			});
+		}
+	}
+});
+
+Template.addSpeaker.rendered = function()
+{
+
+}
--- collections/conferences.js
+++ collections/conferences.js
@@ -16,26 +16,54 @@ Meteor.methods({
 				attributes, 
 				'name'),
 			{
-
+				categories: [],
+				speakers: [],
+				sessions: []
 			});
 
 			var conferenceId = Conferences.insert(newConference);
 			return conferenceId;
 		}
 	},
+	addSpeakerToConference: function(attributes)
+	{
+
+		var conference = Conferences.findOne({_id: attributes.conferenceId});
+
+		if(conference)
+		{
+			var conf = Conferences.update(
+				Conferences.findOne({_id:conference._id}),
+				{
+					$addToSet: 
+					{
+						speakers: 
+						{
+							_id: new Meteor.Collection.ObjectID()._str,
+							name: attributes.name,
+							location: attributes.location,
+							blog: attributes.blog,
+							website: attributes.website,
+							twitter: attributes.twitter,
+							details: attributes.details
+						}
+					}
+				}
+			);
+			
+			return conference._id;
+		}
+		else
+		{
+			return 'asdf';
+		}
+	},
 	addTimeBlockToConference: function(attributes)
 	{
 
 	},
-	addSpeakerToConference: function(attributes)
-	{
-
-	},
+	
 	addCategoryToConference: function(attributes)
-	{
-
-	},
-	addSpeakerToConference: function(attributes)
 	{
 
 	}
