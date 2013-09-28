5a9093f0002a8c5fdfc2679b39cd0416b61666b6
Kyle Smyth <kylesmyth@gmail.com>
2013-09-28 09:29:03 -0600

    Added skeleton html and js files for views

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -7,32 +7,70 @@ Meteor.Router.add({
 			return 'home';
 		}
 	},
-	'/add':
+	'/new':
 	{
-
+		as: 'addConference',
+		to: function()
+		{
+			return 'addConference';
+		}
 	},
 	'/:_id':
 	{
-
+		as: 'viewConference',
+		to: function()
+		{
+			return 'viewConference';
+		}
 	},
 	'/:_id/session/':
 	{
-
+		as: 'addSession',
+		to: function()
+		{
+			Session.set('conferenceId', id);
+			return 'addSession';
+		}
 	},
 	'/:_id/session/:sId':
 	{
+		as: 'viewSession',
+		to: function(id, sId)
+		{
+			Session.set('conferenceId', id);
+			Session.set('sessionId', sId);
+			return 'viewSession';
+		}
 
 	},
 	'/:_id/speaker/new':
 	{
+		as: 'addSpeaker',
+		to: function(id)
+		{
+			Session.set('conferenceId', id);
+			return 'addSpeaker';
+		}
 
 	},
-	':_id/speaker/:id':
+	':_id/speaker/:sId':
 	{
+		as: 'viewSpeaker',
+		to: function(id, sId)
+		{
+			Session.set('conferenceId', id);
+			Session.set('speakerId', sId);
+			return 'viewSpeaker';
+		}
 
 	},
 	':_id/cat/':
 	{
-
+		as: 'category',
+		to: function(id)
+		{
+			Session.set('conferenceId', id);
+			return 'category';
+		}
 	}
 });
